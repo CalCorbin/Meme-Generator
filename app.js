2fc8990c5b4d75a1b0bb79cba2654d11959fffa3
Cal Corbin <calcorbin@gmail.com>
2017-04-19 15:34:26 +0000

    Fixed up the default states so image resets to default  when user deletes captions or image url.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var main = function() {
 
   $('#top-text').keyup(function() {
-    var top = $(this).val();
+    var top = $(this).val() || "U say something?";
     $('.top-caption').text(top);
 });
   $('#bottom-text').keyup(function() {
-    var bottom = $(this).val();
+    var bottom = $(this).val() || "Deal with it";
     $('.bottom-caption').text(bottom)
 });
   $('#image-url').keyup(function() {
-    var image = $(this).val();
+    var image = $(this).val() || "pancham.png";
     $('div.meme > img').attr('src',image);
  });
 };
